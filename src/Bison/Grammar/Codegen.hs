75511fd37d8c7e7bfe9a621c51aff21fd47fb1fa
Justin Christensen <wroathe@gmail.com>
2019-07-28 18:41:44 -0500

    starting to work through the parser logic

--- src/Bison/Grammar/Codegen.hs
+++ src/Bison/Grammar/Codegen.hs
@@ -2,11 +2,12 @@ module Bison.Grammar.Codegen (
     productions
 ) where
 
+import qualified Data.Text.IO as T (getContents)
+import Bison.Grammar.Parser
 import Bison.Grammar.Types
 
 productions :: ([Production] -> IO ()) -> IO ()
 productions f = do
-    grammar <- getContents
-    print grammar
-    f []
+    grammar <- T.getContents
+    f $ parse grammar
 
--- src/Bison/Grammar/Types.hs
+++ src/Bison/Grammar/Types.hs
@@ -1,3 +1,18 @@
 module Bison.Grammar.Types where
 
-data Production = Production
+import Data.Text (Text)
+
+data Production = Production {
+        name :: Text
+    ,   clauses :: [Clause]
+    } deriving (Show, Read, Eq, Ord)
+
+newtype Clause = Clause {
+        tokens :: [Token]
+    } deriving (Show, Read, Eq, Ord)
+
+data Token
+    = Terminal Text
+    | NonTerminal Text
+    | Directive Text
+    deriving (Show, Read, Eq, Ord)
